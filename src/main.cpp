c2bea509bbf9dd9e44f76a7307fd7e76aff752dc
Enriquefft <enriquefft2001@gmail.com>
2022-10-19 07:02:45 -0500

    updated main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    cout << "Hello World!" << endl;
+#include "System.h"
+#include "Config/Config.h"
+
+int main (int argc, char *argv[])
+{
+
+  config_base = new Config();
+  //config_adicional = new Config(bot=true, tiempo=120);
+
+  sistema = new System();
+  sistema->crear_juego(config_base);
+  
+  return 0;
 }
