c86977c72d0db25c953957e056a1d6933c2a2bc4
Enriquefft <enriquefft2001@gmail.com>
2022-10-19 08:02:53 -0500

    iniciar juego pseudocode

--- src/Game/Game.cpp
+++ src/Game/Game.cpp
@@ -1,3 +1,27 @@
 #include <iostream>
 #include "Game.h"
 using namespace std;
+
+
+
+/*
+ Iniciar Juego:
+
+ config_opcional = vista.configurarJuego()
+ if config_opcional != None:
+    cambiar configuracion
+ 
+ while (game is not over)
+ {
+    
+    turno = new turn(tablero)
+
+    turno->play()
+
+    game_over = turno->game_over()
+
+ }
+
+ vista.mostrar_resultado()
+
+*/
