6e3e65f39bffd35ab36d8fa7af047b9965becf19
Enriquefft <enriquefft2001@gmail.com>
2022-10-19 07:26:25 -0500

    finished Player

--- src/Player/Player.cpp
+++ src/Player/Player.cpp
@@ -1,3 +1,23 @@
-#include <iostream>
 #include "Player.h"
-using namespace std;
+
+void Player::ChangeName(std::string new_name){
+  this->m_name = new_name;
+}
+
+void Player::ChangeColor(char new_color){
+  this->m_color = new_color;
+}
+
+bool Player::IsWinner(){
+  return this->m_is_winner;
+}
+
+/*
+std::string Player::GetName(){
+  return this->m_name;
+}
+
+char Player::GetColor(){
+  return this->m_color;
+}
+*/
--- src/Player/Player.h
+++ src/Player/Player.h
@@ -4,16 +4,18 @@
 #define PLAYER_H
 
 class Player {
+
 private:
 
-std::string name;
-bool is_winner;
-char color;
+std::string m_name;
+bool m_is_winner;
+char m_color;
 
 public:
 
 void ChangeName(std::string new_name);
 void ChangeColor(char new_color);
+bool IsWinner();
 
 };
 
